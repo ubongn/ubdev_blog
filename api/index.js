e034b9cf25c819a57408e6a1504e856fc154bb61
ubong <ubongnt@gmail.com>
2024-01-30 14:39:11 +0100

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "../api/routes/user.route.js"
 
 dotenv.config();
 
@@ -15,7 +16,8 @@ mongoose
 
 const app = express();
 
-
 app.listen(3000, () => {
     console.log("listening on post 3000")
 })
+
+app.use('/api/user', userRoutes);
