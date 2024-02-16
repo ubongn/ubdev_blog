2b61729425e1eccbb8ff1f45da3d18de6a3e932a
ubong <ubongnt@gmail.com>
2024-02-16 09:20:25 +0100

    add redux toolkit

--- api/index.js
+++ api/index.js
@@ -1,16 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from "../api/routes/user.route.js"
-import authRoutes from "../api/routes/auth.route.js"
+import userRoutes from "../api/routes/user.route.js";
+import authRoutes from "../api/routes/auth.route.js";
 
 dotenv.config();
-
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log('MongoDb is connected');
+    console.log("MongoDb is connected");
   })
   .catch((err) => {
     console.log(err);
@@ -21,19 +20,18 @@ const app = express();
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log("listening on post 3000")
-})
+  console.log("listening on port 3000");
+});
 
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
-
-app.use((err, req,res,next) => {
-   const statusCode = err.statusCode || 500;
-   const message = err.message || 'Internal Server Error';
-   res.status(statusCode).json({
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
     success: false,
     statusCode,
     message,
-   });
-})
+  });
+});
