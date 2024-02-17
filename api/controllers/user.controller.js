6c91c23c8fb1bd7fa20475184b721edca8658c2c
ubong <ubongnt@gmail.com>
2024-02-17 03:06:17 +0100

    complete update user profile page functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,6 @@
-import bcryptjs from 'bcryptjs';
-import { errorHandler } from '../utils/error.js';
-import User from '../models/user.model.js';
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import User from "../models/user.model.js";
 
 export const test = (req, res) => {
   res.json({ message: "API is working!" });
