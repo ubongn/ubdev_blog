673cdd324830f6d1a91faafdf433f08947c54ec8
ubong <ubongnt@gmail.com>
2024-02-17 13:27:46 +0100

    create get users api route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
+import PostPage from "./pages/PostPage";
 
 function App() {
   return (
@@ -31,6 +32,7 @@ function App() {
         </Route>
 
           <Route path="/projects" element={<Projects />} />
+          <Route path='/post/:postSlug' element={<PostPage />} />
         </Routes>
         <Footer />
       </BrowserRouter>
